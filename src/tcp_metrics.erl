096f8ff59ceedbfd320d1ae5b50c45f496851c2f
Julian Squires <julian@cipht.net>
2016-02-22 11:22:21 -0500

    Permit filtering by source address
    As a step towards reducing the size of the table over time.

--- src/tcp_metrics.erl
+++ src/tcp_metrics.erl
@@ -9,22 +9,34 @@
 -type ip() :: 0..4294967295.
 -type rtt() :: integer().
 
+-record(state, {
+    source_address :: inet:ip4_address(),
+    port :: port(),
+    buffer :: binary()
+}).
+
 %%% PUBLIC
 
 -spec start_link() -> {ok, pid()} | {error, term()}.
-start_link() -> proc_lib:start_link(?MODULE, init, [self()]).
+start_link() ->
+    proc_lib:start_link(?MODULE, init, [self()]).
 
 
 -spec init(pid()) -> ok.
 init(Parent) ->
     proc_lib:init_ack(Parent, {ok, self()}),
     ets:new(?TABLE, [set, {read_concurrency, true}, named_table]),
-    loop(create_port(), <<>>).
+    SourceAddress =
+        case application:get_env(tcp_metrics, source_address) of
+            {ok, X} -> X;
+            _ -> {0,0,0,0}
+        end,
+    loop(#state{source_address = SourceAddress}).
 
 
 %% lookup the estimated RTT in millisecs for an IP.
 -spec lookup(ip() |
-             {0..255,0..255,0..255,0..255} |
+             inet:ip4_address() |
              binary())
             -> rtt() | undefined.
 lookup(Ip) when is_integer(Ip) ->
@@ -56,19 +68,28 @@ priv_dir() ->
     end.
 
 
--spec create_port() -> port().
-create_port() ->
-    open_port({spawn, port_path("tcp_metrics_port")},
-              [binary, exit_status]).
+-spec string_of_ip4_address(inet:ip4_address()) -> string().
+string_of_ip4_address(A) ->
+    string:join([integer_to_list(O) || O <- tuple_to_list(A)], ".").
 
 
--spec loop(port(), binary()) -> ok.
-loop(Port, Buffer) ->
+-spec create_port(inet:ip4_address()) -> port().
+create_port(SourceAddress) ->
+    Args = ["5", string_of_ip4_address(SourceAddress)],
+    open_port({spawn_executable, port_path("tcp_metrics_port")},
+              [binary, exit_status, {args, Args}]).
+
+
+-spec loop(#state{}) -> ok.
+loop(#state{port = undefined, source_address = SourceAddress} = State) ->
+    loop(State#state{port = create_port(SourceAddress), buffer = <<>>});
+loop(#state{port = Port,
+            buffer = Buffer} = State) ->
     receive
         {_Port, {data, In}} ->
-            loop(Port, process(<<Buffer/bytes, In/bytes>>));
+            loop(State#state{buffer = process(<<Buffer/bytes, In/bytes>>)});
         {_Port, {exit_status, _Status}} ->
-            loop(create_port(), <<>>);
+            loop(State#state{port = undefined, buffer = undefined});
         stop ->
             port_close(Port),
             ok
